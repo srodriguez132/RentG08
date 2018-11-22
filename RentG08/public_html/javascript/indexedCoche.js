603e74659556b1578a64421e0540ee872f9a3b1a
AndoniD <kurri@10.111.99.171>
2018-11-22 11:57:14 +0100

    indexedcoches

--- RentG08/public_html/javascript/indexedCoche.js
+++ RentG08/public_html/javascript/indexedCoche.js
@@ -1,12 +1,42 @@
 // JavaScript Document
 var bd;
+function agregarObjeto() {
+        var transaccion = bd.transaction(["coches"], "readwrite");
+
+        var almacen = transaccion.objectStore("coches");
+        var agregar;
+
+        agregar = almacen.add[{matricula: "1111aaa", marca: "BMW", imagen: "//img/bmw.png"},
+                {matricula: "2222bbb", marca: "Citroen", imagen: "//img/citroen.png"},
+                {matricula: "3333ccc", marca: "Ford", imagen: "//img/ford.png"},
+                {matricula: "4444dd", marca: "Mercedes", imagen: "//img/mercedes.png"}];
+//        agregar.onsuccess = function (e) {
+//            alert('Registro completado correctamente');
+////                   location.href="altaPacientes.html";
+//        };
+
+//        agregar.onerror = function (e) {
+//            alert('Registro fallido');
+////               location.href="altaPacientes.html";
+//        };
+    }
 function iniciar() {
 
     var solicitud = indexedDB.open("RentG08");
 
     solicitud.onsuccess = function (e) {
         bd = e.target.result;
-        agregarObjeto();
+        var transaccion = bd.transaction(["coches"], "readwrite");
+
+        var almacen = transaccion.objectStore("coches");
+        var agregar;
+        const datos = [{matricula: "1111aaa", marca: "BMW", imagen: "//img/bmw.png"},
+                {matricula: "2222bbb", marca: "Citroen", imagen: "//img/citroen.png"},
+                {matricula: "3333ccc", marca: "Ford", imagen: "//img/ford.png"},
+                {matricula: "4444dd", marca: "Mercedes", imagen: "//img/mercedes.png"}];
+     for (var i in datos) {
+         almacen.add(datos[i]);
+    }
     };
 
     solicitud.onerror = function (e) {
@@ -21,26 +51,5 @@ function iniciar() {
 }
     
 
-    function agregarobjeto() {
-        var transaccion = bd.transaction(["coches"], "readwrite");
-
-        var almacen = transaccion.objectStore("coches");
-        var agregar;
-
-        agregar = almacen.add({matricula: "1111aaa", marca: "BMW", imagen: "//img/bmw.png"},
-                {matricula: "2222bbb", marca: "Citroen", imagen: "//img/citroen.png"},
-                {matricula: "3333ccc", marca: "Ford", imagen: "//img/ford.png"},
-                {matricula: "4444dd", marca: "Mercedes", imagen: "//img/mercedes.png"});
-        agregar.onsuccess = function (e) {
-            alert('Registro completado correctamente');
-//                   location.href="altaPacientes.html";
-        };
-
-        agregar.onerror = function (e) {
-            alert('Registro fallido');
-//               location.href="altaPacientes.html";
-        };
-    }
-    ;
-
+;
 window.addEventListener("load", iniciar);
