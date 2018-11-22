c0f8fd2ce1ff6ac002cafa7d2809b6917868e24f
AndoniD <kurri@10.111.99.171>
2018-11-22 11:58:01 +0100

    indexedcoches

--- RentG08/public_html/javascript/indexedCoche.js
+++ RentG08/public_html/javascript/indexedCoche.js
@@ -1,25 +1,5 @@
 // JavaScript Document
 var bd;
-function agregarObjeto() {
-        var transaccion = bd.transaction(["coches"], "readwrite");
-
-        var almacen = transaccion.objectStore("coches");
-        var agregar;
-
-        agregar = almacen.add[{matricula: "1111aaa", marca: "BMW", imagen: "//img/bmw.png"},
-                {matricula: "2222bbb", marca: "Citroen", imagen: "//img/citroen.png"},
-                {matricula: "3333ccc", marca: "Ford", imagen: "//img/ford.png"},
-                {matricula: "4444dd", marca: "Mercedes", imagen: "//img/mercedes.png"}];
-//        agregar.onsuccess = function (e) {
-//            alert('Registro completado correctamente');
-////                   location.href="altaPacientes.html";
-//        };
-
-//        agregar.onerror = function (e) {
-//            alert('Registro fallido');
-////               location.href="altaPacientes.html";
-//        };
-    }
 function iniciar() {
 
     var solicitud = indexedDB.open("RentG08");
@@ -33,7 +13,7 @@ function iniciar() {
         const datos = [{matricula: "1111aaa", marca: "BMW", imagen: "//img/bmw.png"},
                 {matricula: "2222bbb", marca: "Citroen", imagen: "//img/citroen.png"},
                 {matricula: "3333ccc", marca: "Ford", imagen: "//img/ford.png"},
-                {matricula: "4444dd", marca: "Mercedes", imagen: "//img/mercedes.png"}];
+                {matricula: "4444ddd", marca: "Mercedes", imagen: "//img/mercedes.png"}];
      for (var i in datos) {
          almacen.add(datos[i]);
     }
