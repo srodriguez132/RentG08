ac6e77c0b85e84e302194a6f9f1124e46b4ee655
Usuario <Usuario@10.111.16.19>
2018-11-27 18:29:44 +0100

    consulta usuario terminada

--- RentG08/public_html/javascript/indexedunido.js
+++ RentG08/public_html/javascript/indexedunido.js
@@ -282,10 +282,11 @@ function mostrarDatosDespues(e) {
 
     var cursor = e.target.result;
     if (cursor) {
-        if (cursor.value.fechaI > document.getElementById("fechaUsuario") && cursor.value.email === sessionStorage.getItem("email")) {
+        if (cursor.value.fechaI > document.getElementById("fechaUsuario").value && cursor.value.email === sessionStorage.getItem("email")) {
             var fechaHoraI = new Date(cursor.value.fechaI + ' ' + cursor.value.horaI);
         var fechaHoraF = new Date(cursor.value.fechaF + ' ' + cursor.value.horaF);
-            cajaReservasUsuario.innerHTML += "<div>" +  cursor.value.email + " - " + cursor.value.matricula + " - " +fechaHoraI + " - " + fechaHoraF + " - " + cursor.value.lugar + "</div>";
+            cajaReservasUsuario.innerHTML += "<div>" +  cursor.value.email + " - " + cursor.value.matricula
+                    + " - " +fechaHoraI + " - " + fechaHoraF + " - " + cursor.value.lugar + "</div>";
         }
         cursor.continue();
     }
